171caad826ecd18a9707a21fd17ba7b42ec3fbfa
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-08-18 16:11:26 +0100

    Add websocket support
    We require websocket support, specifically the ability to be able to
    send updated state to some or all clients. This commit adds this
    ability.
    
    Includes:
    - Adds ws module
    - WebSocketServer class, provides handling of client connections,
    disconnections and provides methods to allow broadcasting or more
    selective sending of messages
    - Initialisation of WS server upon server start

---
+++ helpers/WebsocketServer.js
@@ -0,0 +1,71 @@
+const WebSocket = require('ws');
+const { verifyJwt } = require('./token');
+
+class WebsocketServer {
+    init(server) {
+        const { WS_PORT } = process.env;
+
+        // Initialise the server
+        this.socketServer = new WebSocket.Server({ port: WS_PORT });
+        console.log(`Websocket server running on port ${WS_PORT}`);
+        this.handleConnectDisconnect();
+    }
+
+    handleConnectDisconnect() {
+        // Handle connection and disconnection of client
+        this.socketServer.on('connection', (socketClient) => {
+            // Ensure the connection contains a valid JWT. We're
+            // bastardising the 'Sec-WebSocket-Protocol' header here
+            // to pass a JWT
+            const jwt = socketClient.protocol;
+            try {
+                // If we've got a valid JWT
+                const { sub } = verifyJwt(jwt);
+                // Get the user ID in the payload and add it
+                // to the client object, this will enable us to
+                // target messages
+                socketClient.userId = sub;
+                console.log(`WS client connected, ${this.socketServer.clients.size} clients connected`);
+                socketClient.on('close', () => {
+                    console.log(`WS client disconnected, ${this.socketServer.clients.size} clients connected`);
+                });
+            } catch (err) {
+                console.log('WS client unauthorised, closing connection');
+                socketClient.close();
+            }
+        })
+    }
+    // Send a message to all available clients
+    // initiator: the ID of the user that initiated the action (if available)
+    // subject: The subject of the operation that the action was on (e.g. message)
+    // action: The action that was performed on the subject (e.g. update)
+    // payload: Any payload resulting from the action on the subject (e.g. updated message)
+    broadcastMessage({ initiator, subject, action, payload }) {
+        this.socketServer.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(
+                    JSON.stringify({ initiator, subject, action, payload })
+                );
+            }
+        });
+    }
+    // Send a message to all client *except* the one that initiated
+    // the action
+    excludeInitiatorMessage({ initiator, subject, action, payload }) {
+        this.socketServer.clients.forEach((client) => {
+            if (
+                parseInt(client.userId) !== parseInt(initiator) &&
+                client.readyState === WebSocket.OPEN
+            ) {
+                client.send(
+                    JSON.stringify({ initiator, subject, action, payload })
+                );
+            }
+        });
+
+    }
+}
+
+const singleton = new WebsocketServer();
+
+module.exports = singleton;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const db = require('../ems-db');
 const ems = require('./app');
+const WebsocketServer = require('./helpers/WebsocketServer');
 
 // Make sure the database is fully up to date before
 // starting the API
@@ -12,6 +13,7 @@ db.update()
             app.listen(API_PORT, () => {
                 console.log(`Server running on port ${API_PORT}`);
             });
+            WebsocketServer.init(app);
         });
     })
     .error((e) => console.log(e));
