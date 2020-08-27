8c48e479643df8b0e2ccf715f66b41e830a42c89
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-08-27 12:23:55 +0100

    Remove addUserToQuery
    This is deprecated since we no longer explicitly add a user to a query,
    they are added when the contribute a message to a query
    
    Includes:
    - Removes queryuser.addUserToQuery()
    - Removes associated unit test

--- controllers/queryuser.js
+++ controllers/queryuser.js
@@ -1,21 +1,6 @@
 const db = require('../../ems-db');
 
 const queryuser = {
-    addUserToQuery: (req, res, next) =>
-        db.resolvers.queryuser
-            .addUserToQuery(req)
-            .then((result) => {
-                if (result.rowCount === 1) {
-                    res.status(201);
-                    res.json(result.rows[0]);
-                    next();
-                } else {
-                    res.status(500);
-                    res.send();
-                    next();
-                }
-            })
-            .catch((err) => next(err)),
     updateMostRecentSeen: (req, res, next) =>
         db.resolvers.queryuser
             .updateMostRecentSeen(req)
--- test/controllers/queryuser.test.js
+++ test/controllers/queryuser.test.js
@@ -12,17 +12,6 @@ jest.mock('../../../ems-db', () => ({
         queryuser: {
             // A mock DB resolver that returns a promise that resolves
             // to whatever it was passed
-            addUserToQuery: jest.fn((passed) => {
-                if (passed) {
-                    return new Promise((resolve) => {
-                        return resolve(passed);
-                    });
-                } else {
-                    return new Promise((resolve, reject) => {
-                        return reject(new Error('Rejected'));
-                    });
-                }
-            }),
             // A mock DB resolver that returns a promise that resolves
             // to whatever it was passed
             updateMostRecentSeen: jest.fn((passed) => {
@@ -41,84 +30,6 @@ jest.mock('../../../ems-db', () => ({
 }));
 
 describe('QueryUsers', () => {
-    describe('addUserToQuery', () => {
-        // res.json is used, so we should mock that
-        const res = { json: jest.fn(), send: jest.fn(), status: jest.fn() };
-        // Mock next so we can check it has been called
-        const next = jest.fn();
-
-        // Make the === 0 call
-        // Here we're telling our mocked addUserToQuery DB resolver above to
-        // pretend it's not inserted a user query relationship
-        queryuser.addUserToQuery({ rowCount: 0 }, res, next);
-
-        it('should call the DB resolver', (done) => {
-            expect(db.resolvers.queryuser.addUserToQuery).toHaveBeenCalled();
-            done();
-        });
-        it('rowCount === 0 should call status(), passing 500', (done) => {
-            expect(res.status).toBeCalledWith(500);
-            done();
-        });
-        it('rowCount === 0 should call send()', (done) => {
-            expect(res.send).toHaveBeenCalled();
-            done();
-        });
-        it('rowCount === 0 should call next()', (done) => {
-            expect(next).toHaveBeenCalled();
-            done();
-        });
-
-        // Make the == 1 call
-        // Here we're telling our mocked addUserToQuery DB resolver above to
-        // pretend it's successfully inserted user query relationship
-        // POST:
-        queryuser.addUserToQuery(
-            { rowCount: 1, rows: [mockResult] },
-            res,
-            next
-        );
-
-        it('rowCount == 1 should call status(), passing 201', (done) => {
-            expect(res.status).toBeCalledWith(201);
-            done();
-        });
-        it('rowCount == 1 should call json()', (done) => {
-            expect(res.json).toHaveBeenCalled();
-            done();
-        });
-        it('rowCount == 1 should call next()', (done) => {
-            expect(next).toHaveBeenCalled();
-            done();
-        });
-
-        // Make the > 1 call
-        // Here we're telling our mocked addUserToQuery DB resolver above to
-        // pretend it's updated > 1 rows which shouldn't ever happen
-        // POST:
-        queryuser.addUserToQuery({ rowCount: 2 }, res, next);
-
-        it('rowCount > 1 should call status(), passing 500', (done) => {
-            expect(res.status).toBeCalledWith(500);
-            done();
-        });
-        it('rowCount > 1 should call send()', (done) => {
-            expect(res.send).toHaveBeenCalled();
-            done();
-        });
-        it('rowCount > 1 should call next()', (done) => {
-            expect(next).toHaveBeenCalled();
-            done();
-        });
-
-        // Make the failed call
-        queryuser.addUserToQuery(false, res, next);
-        it('should call next() from the catch passing the error', (done) => {
-            expect(next).toHaveBeenCalledWith(new Error('Rejected'));
-            done();
-        });
-    });
-
     describe('updateMostRecentSeen', () => {
         // res.json is used, so we should mock that
         const res = { json: jest.fn(), send: jest.fn(), status: jest.fn() };
