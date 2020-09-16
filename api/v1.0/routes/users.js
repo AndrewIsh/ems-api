163e322b150cc495a47b953574c3ebaf4f606164
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-09-16 16:03:27 +0100

    Comment out unused API routes
    We currently have no use for the API routes that allow user
    manipulation, so I have commented them out. The controllers are still
    there, but they are unreachable

--- api/v1.0/routes/users.js
+++ api/v1.0/routes/users.js
@@ -5,8 +5,10 @@ const { checkIsInRole } = require('../../../auth/utils');
 
 router.get('/', checkIsInRole('STAFF', 'CUSTOMER'), users.getUsers);
 router.get('/:id', checkIsInRole('STAFF', 'CUSTOMER'), users.getUser);
+/* Not currently required
 router.post('/', checkIsInRole('STAFF'), users.upsertUser);
 router.put('/:id', checkIsInRole('STAFF'), users.upsertUser);
 router.delete('/:id', checkIsInRole('STAFF'), users.deleteUser);
+*/
 
 module.exports = router;
